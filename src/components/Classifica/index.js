d9199db68798f8abb0ca6246464208d77ceb2c8c
nzambello <nicola.zambello@redturtle.it>
2018-03-29 00:33:53 +0200

    added classifica, default value for squadra

---
+++ src/components/Classifica/index.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import Squadra from '../Squadra';
+
+const Classifica = () => (
+  <div id="classifica">
+    <div className="section-list">
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+      <div className="list-item row">
+        <div className="col list-squadra">
+          <Squadra id="team-ploro" teamName="teamA" />
+        </div>
+        <div className="col list-punti">
+          <span>{parseInt(Math.random() * 10)}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Classifica;
--- src/components/Squadra/index.js
+++ src/components/Squadra/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 
-const Squadra = ({ id, teamName, winner }) => (
+const Squadra = ({ id, teamName, winner = false }) => (
   <div className={`squadra${winner ? ' winner' : ''}`}>
     <a href={`/teams/${id}`} title="Team page">
       <span className="team">{teamName}</span>
