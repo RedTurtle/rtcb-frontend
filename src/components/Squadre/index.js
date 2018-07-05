ba528043297c2c97979458c367e55f86ca11e9f6
nzambello <nicola.zambello@redturtle.it>
2018-07-05 20:50:41 +0200

    aggiunta modale di aggiunta

--- src/components/Squadre/index.js
+++ src/components/Squadre/index.js
@@ -3,6 +3,7 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import TeamsList from '../TeamsList';
 import matchSorter from 'match-sorter';
+import AddModal from '../AddModal';
 import './index.css';
 
 const teamsQuery = gql`
@@ -37,13 +38,16 @@ class Squadre extends Component {
     return (
       <div className="squadre container">
         <h1>Squadre</h1>
-        <input
-          value={this.state.teamPattern}
-          onChange={this.onType}
-          placeholder="Cerca una squadra..."
-          type="text"
-          className="form-control"
-        />
+        <div className="search-add-wrapper">
+          <input
+            value={this.state.teamPattern}
+            onChange={this.onType}
+            placeholder="Cerca una squadra..."
+            type="text"
+            className="form-control"
+          />
+          <AddModal btnLabel="squadra" />
+        </div>
         <div className="squadre-list list-group">
           <Query query={teamsQuery}>
             {({ loading, error, data }) => {
